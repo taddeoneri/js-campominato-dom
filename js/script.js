108d19047bfe0bdea8f2f4b845b46cce1ca7049b
taddeoneri <taddeo.neri10@gmail.com>
2023-03-20 15:31:38 +0100

    result

---
+++ js/script.js
@@ -0,0 +1,88 @@
+const formSelect = document.getElementById('formSelect');
+formSelect.addEventListener('submit', play);
+
+
+function createSquare(content, row){
+    const square = document.createElement('div');
+    square.classList.add('square')
+    square.style.width = `calc(100% / ${row})`;
+    square.style.height = `calc(100% / ${row})`;
+    square.innerHTML = content;
+    return square;
+}
+
+
+function createBombs(numbombs, max){
+    const bombs = [];
+    while(bombs.length < numbombs){
+        const bomb = getRandomInt(1, max);
+        if(!bombs.includes(bomb)){
+            bombs.push(bomb);
+        }
+    }
+    return bombs;
+}
+
+
+function setPar(result){
+    const par = document.getElementById('result');
+    par.innerHTML = result;
+}
+
+
+function play(e){
+    e.preventDefault();
+    const table = document.getElementById('table');
+    table.innerHTML = '';
+    const level = document.getElementById('select').value;
+    let squareNumbers;
+    const numBombs = 16;
+    let result = 'Seleziona la difficoltà e premi play.';
+    setPar(result);
+
+    if(level === 'Easy'){
+        squareNumbers = 100;
+    } else if(level === 'Medium'){
+        squareNumbers = 81;
+    } else{
+        squareNumbers = 49;
+    }
+    
+    const bombs = createBombs(numBombs, squareNumbers);
+    console.log(bombs);
+    let row = Math.sqrt(squareNumbers);
+    let maxScore = squareNumbers - numBombs;
+    let par = 0;
+    let gameOver = false;
+
+    for(let i = 1; i <= squareNumbers; i++){
+        const square = createSquare(i, row);
+        square.addEventListener('click', function(){
+            console.log
+            if(!gameOver && !square.classList.contains('safe')){
+                if(bombs.includes(parseInt(square.innerText))){
+                    square.classList.add('unsafe');
+                    result = `Hai perso, il tuo punteggio è: ${par}`;
+                    gameOver = true;
+                } else{
+                    if(par === maxScore){
+                        result = `Hai vinto, il tuo punteggio è: ${score}`;
+                    } else{
+                        square.classList.add('safe');
+                        par++;
+                        result = `Il tuo punteggio è: ${par}`
+                    }
+                }
+                setPar(result);
+            }
+        })
+        table.appendChild(square);
+    }
+    
+}
+
+
+
+function getRandomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
